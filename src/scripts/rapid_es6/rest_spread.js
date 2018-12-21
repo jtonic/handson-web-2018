355b47ba0720ba057801274a1c0488ce6385b624
ws31wx <antonel-ernest.pazargic@ing.com>
2018-12-21 11:45:15 +0200

    rest, spread in ES6

---
+++ src/scripts/rapid_es6/rest_spread.js
@@ -0,0 +1,29 @@
+console.log('============================');
+console.log('rest/spread');
+console.log('============================');
+
+function exec() {
+
+  function income(amount, ...taxes) {
+    console.log(taxes instanceof Array);
+    console.log(taxes);
+  }
+
+  income(2000);
+  income(2000, [0.2, 0.12, 0.3]);
+
+  let arr1 = Array(1, 2, 3);
+  let arr2 = Array(...[, , 3]);
+  let arr3 = Array(...[, , ,]);
+  let arr4 = [...[, , ,]];
+  console.log(arr1);
+  console.log(arr2);
+  console.log(arr3);
+  console.log(arr4);
+
+  console.log(Math.max(...'43210'));
+  let arr5 = ['a',...'bcd', 'e'];
+  console.log(arr5);
+}
+
+export default exec;
--- src/scripts/res6.js
+++ src/scripts/res6.js
@@ -1,6 +1,7 @@
 import af from './rapid_es6/arrow_function';
 import dp from './rapid_es6/default_params';
 import df from './rapid_es6/dynamic_function';
+import rs from './rapid_es6/rest_spread';
 
 console.log('============================');
 console.log('Rapid ECMAScript 6');
@@ -10,6 +11,7 @@ function exec() {
   af();
   dp();
   df();
+  rs();
 }
 
 export default exec;
