9622581d7eabbb5345a57648ae00b8127b87d17b
ws31wx <antonel-ernest.pazargic@ing.com>
2018-12-22 09:55:41 +0200

    object extensions examples

---
+++ src/scripts/rapid_es6/object_extensions.js
@@ -0,0 +1,53 @@
+console.log('============================');
+console.log('object extensions');
+console.log('============================');
+
+function exec() {
+
+  let fName = 'Antonel-Ernest';
+  let lName = 'Pazargic';
+  let age = 'age';
+  let ageValue = 48;
+  let greeting = 'greeting';
+  let mName = 'doId';
+  let fieldName = 'male';
+  //ES 6
+  let o1 = {
+    fName,
+    lName,
+    ['full' + ' ' + 'name']: fName + lName,
+    [age]: ageValue,
+    print() {
+      return `${this.fName} ${this.lName}`;
+    },
+    "print full name"() {
+      return `Full name: ${this.fName} ${this.lName}`;
+    },
+    [greeting]() {
+      return `Hello ${this.print()}`;
+    },
+    [mName + '-001']() {
+      console.log('Do it!!!');
+    },
+    get [fieldName]() { return true; },
+    set [fieldName](value) {  }
+  };
+  console.log(o1, o1.print(), o1["print full name"](), o1[age]);
+  console.log(o1[greeting]());
+  console.log('fullname', o1['f' + 'ull name']);
+  o1[mName + '-001']();
+  console.log(o1.male);
+
+  // ES 5
+  let old1 = {
+    fName:  fName,
+    lName: lName,
+    greeting: function () {
+      console.log(`Hello ${fName} ${lName}`);
+    }
+  };
+  console.log(old1);
+  old1.greeting();
+}
+
+export default exec;
--- src/scripts/res6.js
+++ src/scripts/res6.js
@@ -2,6 +2,7 @@ import af from './rapid_es6/arrow_function';
 import dp from './rapid_es6/default_params';
 import df from './rapid_es6/dynamic_function';
 import rs from './rapid_es6/rest_spread';
+import oe from './rapid_es6/object_extensions';
 
 console.log('============================');
 console.log('Rapid ECMAScript 6');
@@ -12,6 +13,7 @@ function exec() {
   dp();
   df();
   rs();
+  oe();
 }
 
 export default exec;
