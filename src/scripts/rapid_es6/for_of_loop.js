dcc4a35f127b43285edeb42ef862625f946a0aaf
ws31wx <antonel-ernest.pazargic@ing.com>
2018-12-22 10:04:30 +0200

    for of loop examples

---
+++ src/scripts/rapid_es6/for_of_loop.js
@@ -0,0 +1,26 @@
+'use strict';
+
+console.log('============================');
+console.log('[ES6] For of loop');
+console.log('============================');
+
+function exec() {
+
+  const str = 'abcdef';
+  for (const s of str) {
+    console.log(s);
+  }
+  const arr1 = [, ,];
+  let idx = 0;
+  for (const it of arr1) {
+    idx++;
+    console.log(it);
+  }
+  console.log(idx);
+  const arr2 = [1, 2, 3, 4, 5];
+  for (const i of arr2) {
+    console.log(i);
+  }
+}
+
+export default exec;
--- src/scripts/res6.js
+++ src/scripts/res6.js
@@ -3,6 +3,7 @@ import dp from './rapid_es6/default_params';
 import df from './rapid_es6/dynamic_function';
 import rs from './rapid_es6/rest_spread';
 import oe from './rapid_es6/object_extensions';
+import fol from './rapid_es6/for_of_loop';
 
 console.log('============================');
 console.log('Rapid ECMAScript 6');
@@ -14,6 +15,7 @@ function exec() {
   df();
   rs();
   oe();
+  fol();
 }
 
 export default exec;
