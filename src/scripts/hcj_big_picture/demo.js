01f62c36c38dad89554a952177ed74e27c6d29e3
jtonic <antonel.pazargic@gmail.com>
2019-01-02 05:26:02 +0200

    AJAX example

--- src/scripts/hcj_big_picture/demo.js
+++ src/scripts/hcj_big_picture/demo.js
@@ -1,8 +1,25 @@
 "use strict";
 
 export default function () {
+    const demoPar = document.getElementById('demo');
     const showDateBtn = document.getElementById('show-date');
+    const dataFromServerBtn = document.getElementById('data-from-server');
+
     showDateBtn.addEventListener('click', function () {
-        document.getElementById('demo').innerText = Date();
+        const name = window.prompt("Introduce your name");
+        if (name !== null) {
+            demoPar.innerText = `${name}: ${Date()}`;
+        }
+    });
+    dataFromServerBtn.addEventListener('click', function () {
+        let xhttp = new XMLHttpRequest();
+
+        xhttp.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                document.getElementById('demo-data-from-server').innerText = this.responseText;
+            }
+        };
+        xhttp.open("GET", "/public/data_from_server.txt", true);
+        xhttp.send();
     });
 };
